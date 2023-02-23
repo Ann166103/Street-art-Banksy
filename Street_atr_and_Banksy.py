03eb73da7351a8aa13b751f1e3c4088957eafe04
Ann <ankakazaeva@yandex.ru>
2023-02-24 00:32:10 +0300

    download application

---
+++ Street_atr_and_Banksy.py
@@ -0,0 +1,20 @@
+import sys
+
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from street_art import Ui_MainWindow
+
+
+# Наследуемся от виджета из PyQt5.QtWidgets и от класса с интерфейсом
+class MyWidget(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        # Вызываем метод для загрузки интерфейса из класса Ui_MainWindow,
+        # остальное без изменений
+        self.setupUi(self)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
